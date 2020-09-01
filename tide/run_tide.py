2cc1d9391e96ea002663eeb5cc4a7abb1cfaa6eb
thomasyu888 <thomas.yu@sagebase.org>
2020-09-01 11:58:44 -0700

    Add in dockerfile

--- tide/run_tide.py
+++ tide/run_tide.py
@@ -30,7 +30,8 @@ def cli():
     """Build CLI"""
     parser = argparse.ArgumentParser(description='Run Tide')
 
-    parser.add_argument("tpm_file", type=str, help='Synapse username')
+    parser.add_argument("tpm_file", type=str, help='TPM input file')
+    parser.add_argument("output", type=str, help='output prediction file')
 
     return parser.parse_args()
 
@@ -42,26 +43,13 @@ def main():
     tide_cmd = ["tidepy", normalized_tpm_path, "-o", "output.txt", "-c", "NSCLC"]
     subprocess.check_call(tide_cmd)
 
-    tide_scoresdf = pd.read_csv("output.txt")
-    final_prediction = tide_scoresdf[["V1", "TIDE"]]
-
-
-# tide_scores        <- fread("temp_tide_output.txt", data.table = F)[,c("V1","TIDE")]
-# names(tide_scores) <- c("patientID","prediction")
-
-# # make sure the predictions are ordered like the expression data based on patientID.
-# tide_scores        <- tide_scores[match(colnames(seq_dat), tide_scores$patientID),]; rm(seq_dat)
-
-
-# # write out just TIDE scores (not other signatrues like CTL or MDSC)
-# write.csv(tide_scores[,1:2], file = args[2], col.names = T, quote = F, row.names = F)
-
-# file.remove("temp_tide_output.txt")
-# file.remove("log2_mean_normed.txt")
-# print("completed TIDE wrapper")
+    tide_scoresdf = pd.read_table("output.txt", index_col=0)
+    preddf = tide_scoresdf["TIDE"]
+    preddf = preddf.reset_index()
+    preddf.rename(columns={"index": "patientID",
+                           "TIDE": "prediction"}, inplace=True)
+    preddf.to_csv(args.output, index=False)
 
 
 if __name__ == "__main__":
     main()
-
-
